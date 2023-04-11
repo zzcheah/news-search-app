8888741fcaf28a50c9bf18993fd370c85983c8b5
zzcheah <zzcheah@live.com>
2023-04-11 21:32:26 +0800

    Init project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+
+const theme = createTheme();
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                isLoggedIn ? <HomePage /> : <Navigate to="/login" replace />
+              }
+            />
+            <Route
+              path="/login"
+              element={<LoginPage onLogin={() => setIsLoggedIn(true)} />}
+            />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+function HomePage() {
+  return (
+    <Box>
+      <Typography variant="h1">Welcome to the Home Page!</Typography>
+    </Box>
+  );
+}
+
+export default HomePage;
